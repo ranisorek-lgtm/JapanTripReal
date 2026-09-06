7c9d24ff7cce7d572cd68e02d30aeba334216b9b
ranisorek-lgtm <rani.sorek@gmail.com>
2026-09-06 18:38:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template_string, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
@@ -799,4 +800,5 @@ def attractions():
     return render_template_string(LAYOUT, content=content)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
